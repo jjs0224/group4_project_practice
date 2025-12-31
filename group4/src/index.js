ef0c355d2e07b4889e3cabeccbecda90f3893f43
jisu <jisujeon0224@gmail.com>
2025-12-31 15:21:51 +0900

    project
    # Conflicts:
    #	group4/package-lock.json
    #	group4/package.json
    #	group4/src/index.js
    #	jisu.txt

--- group4/src/index.js
+++ group4/src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
+=======
+>>>>>>> ba8547f (project)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+<<<<<<< HEAD
   <BrowserRouter>
     <App />
   </BrowserRouter>
+=======
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+>>>>>>> ba8547f (project)
 );
 
